5a587d65048be77cbfb807863b206f5a083444d5
Michael Burij <michael.burij@gmail.com>
2022-01-20 09:54:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+echo "preparing folders"
 mkdir /$HOME/.nix
 sudo mkdir /nix
+echo "creating deamon"
 sudo cp nix-in-home.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start nix-in-home
 systemctl enable nix-in-home
+echo "launching environment"
 cp bashrc /$HOME/.bashrc
 curl -L https://nixos.org/nix/install | sh
 source ~/.nix-profile/etc/profile.d/nix.sh
