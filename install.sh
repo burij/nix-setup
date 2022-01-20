1aec70c897f5330f3f659a2a43f50b881e57405a
Michael Burij <michael.burij@gmail.com>
2022-01-20 10:30:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,11 @@ sudo cp nix-in-home.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start nix-in-home
 systemctl enable nix-in-home
-echo "launching environment"
+echo "installing environment"
 cp bashrc /$HOME/.bashrc
+echo "installing dependencies"
 sudo apt install curl
 curl -L https://nixos.org/nix/install | sh
 sudo chown -R $USER /nix
+echo "launching environment"
 source ~/.nix-profile/etc/profile.d/nix.sh
